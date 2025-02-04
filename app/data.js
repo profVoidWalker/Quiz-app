d7a4fcc6f70d30360997130d0b8ca14c215c7273
unknown <samuel.mendel@gmail.com>
2025-02-04 21:38:40 +0100

    First commit - files adding

---
+++ app/data.js
@@ -0,0 +1,35 @@
+export const quiz = {
+    totalQuestions: 5,
+    questions: [
+        {
+            id: 1,
+            question: 'What is the capital city of Slovakia?',
+            answers: ['Paris', 'Berlin', 'Prague', 'Bratislava'],
+            correctAnswer: 'Bratislava',
+        },
+        {
+            id: 2,
+            question: 'What is the largest planet in our solar system?',
+            answers: ['Mars', 'Jupiter', 'Venus', 'Saturn'],
+            correctAnswer: 'Jupiter',
+        },
+        {
+            id: 3,
+            question: 'What is the highest mountain in Slovakia?',
+            answers: ['Chopok', 'Kriváň', 'Gerlachovský štít', 'Lomnický štít'],
+            correctAnswer: 'Gerlachovský štít',
+        },
+        {
+            id: 4,
+            question: 'What is the fastest land animal in the world?',
+            answers: ['Cheetah', 'Lion', 'Leopard', 'Tiger'],
+            correctAnswer: 'Cheetah',
+        },
+        {
+            id: 5,
+            question: 'Who is the founder of Microsoft?',
+            answers: ['Steve Jobs', 'Bill Gates', 'Elon Musk', 'Mark Zuckerberg'],
+            correctAnswer: 'Bill Gates',
+        },
+    ]
+}
