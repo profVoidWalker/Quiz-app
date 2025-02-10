6a53de6da5011ae7c71f9c6cb891267cb0eac841
unknown <samuel.mendel@gmail.com>
2025-02-10 22:07:32 +0100

    Update: oprava restart tlačidla a výsledkovej tabule

--- app/quiz/page.jsx
+++ app/quiz/page.jsx
@@ -58,56 +58,59 @@ const page = () => {
       <h1>Quiz Page</h1>
 
       <div>
-        <h2>
-          Question: {activeQuestion + 1}
-          <span>/{questions.length}</span>
-        </h2>
-      </div>
-
-      <div>
         {!showResult ? (
-          <div className='quiz-container'>
-            <h3>{questions[activeQuestion].question}</h3>
-            {answers.map((answer, idx) => (
-              <li 
-                key={idx}
-                onClick={() => onAnswerSelected(answer, idx)}
-                className={
-                  selectedAnswerIndex === idx ? 'li-selected' : 'li-hover'
-                }
-              >
-                <span>{answer}</span>
-              </li>
-            ))}
+          <div>
+            <h2>
+              Question: {activeQuestion + 1}
+              <span>/{questions.length}</span>
+            </h2>
 
-            {checked ? (
-              <button onClick={nextQuestion} className='btn'>
-                {activeQuestion === question.length - 1 ? 'Finish' : 'Next'}
-              </button>
-            ) : (
-              <button onClick={nextQuestion} disabled className='btn-disabled'>
-                {' '}
-                {activeQuestion === question.length - 1 ? 'Finish' : 'Next'}
-              </button>
-            )}
+            <div className='quiz-container'>
+              <h3>{questions[activeQuestion].question}</h3>
+              {answers.map((answer, idx) => (
+                <li 
+                  key={idx}
+                  onClick={() => onAnswerSelected(answer, idx)}
+                  className={
+                    selectedAnswerIndex === idx ? 'li-selected' : 'li-hover'
+                  }
+                >
+                  <span>{answer}</span>
+                </li>
+              ))}
+
+              {checked ? (
+                <button onClick={nextQuestion} className='btn'>
+                  {activeQuestion === question.length - 1 ? 'Finish' : 'Next'}
+                </button>
+              ) : (
+                <button onClick={nextQuestion} disabled className='btn-disabled'>
+                  {' '}
+                  {activeQuestion === question.length - 1 ? 'Finish' : 'Next'}
+                </button>
+              )}
+            </div>
           </div>
           ) : (
-          <div className='quiz-container'>
-            <h3>Results</h3>
-            <h3>Overall {(result.score / 25) * 100}%</h3>
-            <p>
-              Total Questions: <span>{questions.length}</span>
-            </p>
-            <p>
-              Total score: <span>{result.score}</span>
-            </p>
-            <p>
-              Correct Answers: <span>{result.correctAnswers}</span>
-            </p>
-            <p>
-              Wrong Answers: <span>{result.wrongAnswers}</span>
-            </p>
-            <button onClick={() => window.location.reload}>Restart</button>
+          <div>
+            <h2>Results</h2>
+          
+            <div className='quiz-container'>
+              <h3>Overall {(result.score / 25) * 100}%</h3>
+              <p>
+                Total Questions: <span>{questions.length}</span>
+              </p>
+              <p>
+                Total score: <span>{result.score}</span>
+              </p>
+              <p>
+                Correct Answers: <span>{result.correctAnswers}</span>
+              </p>
+              <p>
+                Wrong Answers: <span>{result.wrongAnswers}</span>
+              </p>
+              <button onClick={() => window.location.reload()}>Restart</button>
+            </div>
           </div>
         )}
       </div>
